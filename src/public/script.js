ffd8669297bb08a6ed487f22db0975a77ae4d953
Dawid Kardacz <dkardacz561@gmail.com>
2023-06-22 15:43:02 +0200

    Page with async communication

---
+++ src/public/script.js
@@ -0,0 +1,288 @@
+var rentedBooks = [];
+
+function rentBooks(){
+  var xhr = new XMLHttpRequest();
+  
+  xhr.addEventListener("load", function(evt){
+    var response = JSON.parse(xhr.responseText);
+    if(response.error){
+      displayError(response.errorMSG);
+    }
+    else{
+      rentedBooks = []
+      response.rentedBooks.forEach((book) => {
+          updateBookContainer(book.book);
+        });
+        
+        updateBasket();
+    }
+  });
+
+  xhr.addEventListener("error", function(evt){
+      window.alert('There was a problem with this request.');
+  });
+  xhr.open('POST',"http://localhost:8000/rentBooks", true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({"rentedBooks": rentedBooks}));
+}
+
+
+function addBook(id){
+    const xhr = new XMLHttpRequest();
+    
+    
+    xhr.addEventListener("load", function(evt){
+        var response = JSON.parse(xhr.responseText);
+        if(response.error){
+            console.log(response.errorMSG);
+            displayError(response.errorMSG);
+        }
+        else{
+            if(rentedBooks.length == 0){
+                var newPos = {
+                    'book': response.book,
+                    'no_copies': 1,
+                }
+            
+                rentedBooks.push(newPos);
+            }
+            else{
+                const foundBook = rentedBooks.find(obj => obj.book._id === response.book._id);
+                if (foundBook && response.book.no_copies - foundBook.no_copies > 0) {
+                    foundBook.no_copies+=1;
+                  }
+                else if(!foundBook){
+                    var newPos = {
+                        'book': response.book,
+                        'no_copies': 1,
+                    }
+                
+                    rentedBooks.push(newPos);
+                }
+                else{
+                  msg = "Nie możesz dodać więcej książek tego rodzaju do koszyka"
+                  displayError(msg);
+                }
+            }
+            updateBasket();
+            updateBookContainer(response.book);
+        }
+    });
+
+    xhr.addEventListener("error", function(evt){
+        window.alert('There was a problem with this request.');
+    });
+    xhr.open('POST',"http://localhost:8000/addBookToBasket", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({"id": id}));
+}
+
+function deleteBook(id){
+    const xhr = new XMLHttpRequest();
+    
+    
+    xhr.addEventListener("load", function(evt){
+        var response = JSON.parse(xhr.responseText);
+        const foundBook = rentedBooks.find(obj => obj.book._id === response.book._id);
+        if(foundBook.no_copies > 1){
+          foundBook.no_copies -=1;
+        }
+        else{
+          const index = rentedBooks.findIndex(obj => obj === foundBook);
+          rentedBooks.splice(index,1);
+        }
+        updateBasket();
+        updateBookContainer(response.book);
+    });
+
+    xhr.addEventListener("error", function(evt){
+        window.alert('There was a problem with this request.');
+    });
+    xhr.open('POST',"http://localhost:8000/deleteBookFromBasket", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({"id": id}));
+}
+
+function returnBook(id){
+  const xhr = new XMLHttpRequest();
+    
+    
+    xhr.addEventListener("load", function(evt){
+      var response = JSON.parse(xhr.responseText);
+      if(response.error){
+        displayError(response.errorMSG);
+      }
+      else{
+        updateRentedList();
+      }
+    });
+
+    xhr.addEventListener("error", function(evt){
+        window.alert('There was a problem with this request.');
+    });
+    xhr.open('POST',"http://localhost:8000/returnBook", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({"id": id}));
+}
+
+function updateBasket() {
+    const basketDiv = document.querySelector('.basketList');
+    basketDiv.innerHTML = '';
+  
+    if (rentedBooks && rentedBooks.length > 0) {
+      rentedBooks.forEach((rbook) => {
+        var basketContainer = document.createElement('div');
+        basketContainer.classList.add("basketContainer");
+        var basketimg = document.createElement('img');
+        basketimg.src = rbook.book.url;
+
+        basketimg.addEventListener('click', function() {
+            deleteBook(rbook.book._id);
+          });
+
+        var basketCount = document.createElement('p');
+        basketCount.innerText = rbook.no_copies;
+
+        basketContainer.appendChild(basketimg);
+        basketContainer.appendChild(basketCount);
+        basketDiv.appendChild(basketContainer);
+      });
+        var rentButton = document.createElement('button');
+        rentButton.textContent = 'Wypożycz';
+        rentButton.addEventListener('click', () => rentBooks(rentedBooks));
+        basketDiv.appendChild(rentButton);
+    } else {
+      const p = document.createElement('p');
+      p.textContent = 'No books in basket';
+      basketDiv.appendChild(p);
+    }
+  }
+
+function updateBookContainer(book){
+  const xhr = new XMLHttpRequest();
+    
+    
+    xhr.addEventListener("load", function(evt){
+      var foundBookConainter = document.querySelector("#book" + String(book._id));
+      var foundBookCopies = foundBookConainter.querySelector(".copies");
+      var updatedBook = JSON.parse(xhr.responseText).book;
+      if (rentedBooks && rentedBooks.length > 0) {
+        var foundBook = rentedBooks.find(obj => obj.book._id === book._id);
+        if(foundBook){
+          var leftBooks = book.no_copies - foundBook.no_copies;
+          foundBookCopies.innerText = "Liczba wolnych egzemplarzy: " + leftBooks + "/" + updatedBook.no_copies;
+        }
+        else{
+          foundBookCopies.innerText = "Liczba wolnych egzemplarzy: " + updatedBook.no_copies;
+        }
+    
+        if(leftBooks == 0){
+          foundBookConainter.classList.add('noCopies');
+        } 
+        else{
+          foundBookConainter.classList.remove('noCopies');
+        }
+    
+      }
+      else{
+        foundBookCopies.innerText = "Liczba wolnych egzemplarzy: " + updatedBook.no_copies;
+      }
+    });
+
+    xhr.addEventListener("error", function(evt){
+        window.alert('There was a problem with this request.');
+    });
+    xhr.open('POST',"http://localhost:8000/updateBookContainer", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({"id": book._id}));
+}
+
+function updateRentedList(){
+  const xhr = new XMLHttpRequest();
+    
+    
+  xhr.addEventListener("load", function(evt){
+    var response = JSON.parse(xhr.responseText);
+    if(response.error){
+      displayError(response.errorMSG);
+    }
+    else{
+     var rentedBooks = response.rentedBooks;
+     var bookPanel = document.querySelector(".bookPanel");
+     bookPanel.innerHTML = '';
+     var h = document.createElement('h1');
+     h.innerText = "Wypożyczone przez ciebie książki";
+     bookPanel.appendChild(h);
+     
+
+     rentedBooks.forEach((rbook) => {
+      // Create the book container
+      const bookDiv = document.createElement('div');
+      bookDiv.setAttribute('id', `book${rbook.book._id}`);
+      bookDiv.classList.add('bookContainer');
+  
+      // Create the book image element
+      const bookImg = document.createElement('img');
+      bookImg.classList.add('book');
+      bookImg.setAttribute('src', rbook.book.url);
+      bookImg.setAttribute('alt', rbook.book.title);
+  
+      // Create the info panel
+      const infoPanel = document.createElement('div');
+      infoPanel.classList.add('infoPanel');
+  
+      // Create the book details elements
+      const titleParagraph = document.createElement('p');
+      titleParagraph.textContent = `Tytuł: ${rbook.book.title}`;
+  
+      const authorParagraph = document.createElement('p');
+      authorParagraph.textContent = `Autor: ${rbook.book.author}`;
+  
+      const copiesParagraph = document.createElement('p');
+      copiesParagraph.classList.add('copies');
+      copiesParagraph.textContent = `Liczba wypożyczonych egzemplarzy: ${rbook.no_copies}`;
+  
+      // Create the return button
+      const returnButton = document.createElement('button');
+      returnButton.setAttribute('type', 'button');
+      returnButton.textContent = 'Oddaj';
+      returnButton.addEventListener('click', () => {
+        returnBook(rbook.book._id);
+      });
+  
+      // Append elements to their respective parents
+      infoPanel.appendChild(titleParagraph);
+      infoPanel.appendChild(authorParagraph);
+      infoPanel.appendChild(copiesParagraph);
+      infoPanel.appendChild(returnButton);
+  
+      bookDiv.appendChild(bookImg);
+      bookDiv.appendChild(infoPanel);
+  
+      bookPanel.appendChild(bookDiv);
+    });
+    }
+  });
+
+  xhr.addEventListener("error", function(evt){
+      window.alert('There was a problem with this request.');
+  });
+  xhr.open('POST',"http://localhost:8000/updateRentedList", true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send();
+}
+
+function displayError(errorMSG){
+  const errorPanel = document.querySelector('.errorPanel');
+            var newError = document.createElement('h1');
+            newError.innerHTML = errorMSG;
+            newError.classList.add('errorPanel');
+            errorPanel.appendChild(newError);
+
+            setTimeout(function() {
+                newError.style.opacity = '0';
+                newError.addEventListener('transitionend', function() {
+                  errorPanel.removeChild(newError);
+                });
+              }, 3000);
+}
---
+++ src/server.js
@@ -0,0 +1,291 @@
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import {MongoClient} from 'mongodb';
+import bodyParser from 'body-parser';
+
+/* *************************** */
+/* Configuring the application */
+/* *************************** */
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('views', __dirname + '/views'); // Files with views can be found in the 'views' directory
+app.set('view engine', 'pug'); // Use the 'Pug' template system
+app.locals.pretty = app.get('env') === 'development'; // The resulting HTML code will be indented in the development environment
+app.use(express.urlencoded({ extended: false }));
+/* ************************************************ */
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public')); 
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
+const firstname = "Jan"
+const lastname = "Kowalski"
+
+/* ******** */
+/* "Routes" */
+/* ******** */
+
+app.get('/', async function (request, response, next) {
+    const client = new MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2');
+    await client.connect();
+    const db = client.db('AGH');
+    const collection = db.collection('books');
+    const docs = await collection.find({'_id': {$lt: 5}}).toArray();
+    response.render('mainPage', {'mainBooks': docs}); // Render the 'index' view
+});
+
+app.get('/books', async function (request, response, next) {
+    const client = new MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2');
+    await client.connect();
+    const db = client.db('AGH');
+    const collection = db.collection('books');
+    const docs = await collection.find({}).toArray();
+    response.render('booksList', {'books': docs}); // Render the 'index' view
+});
+
+app.post('/updateBookContainer', async function (request, response, next) {
+    const client = new MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2');
+    await client.connect();
+    const db = client.db('AGH');
+    const books = db.collection('books');
+    var book = await books.findOne({"_id": request.body.id});
+    response.send({"book": book});
+});
+
+app.post('/rentBooks', async function (request, response, next) {
+    const client = new MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2');
+    await client.connect();
+    const db = client.db('AGH');
+    const books = db.collection('books');
+    const students = db.collection('students');
+    const rentals = db.collection('rentals');
+
+    var rentedBooks = request.body.rentedBooks;
+    var error = false;
+    var errorMSG = '';
+
+    var student = await students.findOne({"firstname" :firstname, "lastname" :lastname});
+    if(student){
+        rentedBooks.forEach(async rbook => {
+            var book = rbook.book;
+            for(var i = 0; i < rbook.no_copies; i++){
+                await rentals.insertOne({'book_id': book._id, 'student_id': student._id});
+            }
+            await books.updateOne(
+            { '_id': book._id },
+            { $set: { 'no_copies': book.no_copies-rbook.no_copies } }
+            );
+        });
+    }
+    else{
+        error = true;
+        errorMSG = "Nie znaleziono studenta"
+    }
+    response.send({"rentedBooks": rentedBooks, "error": error, "errorMSG": errorMSG});
+});
+
+app.post('/addBookToBasket', async function (request, response, next) {
+    const client = new MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2');
+    await client.connect();
+    const db = client.db('AGH');
+    const books = db.collection('books');
+    const students = db.collection('students');
+    const rentals = db.collection('rentals');
+              
+    const id = parseInt(request.body.id);
+
+    var error = false;
+    var errorMSG = '';
+    var book = null;
+ 
+    const student = await students.findOne({'firstname': firstname, 'lastname': lastname});
+    if(!student){
+            error = true;
+            errorMSG = "Podany student nie znajduje się w naszej bazie";
+        }
+        else{
+            book = await books.findOne({'_id': id});  
+            if(!book){
+                    error = true;
+                    errorMSG = "Podanej książki nie ma w naszej bazie";
+                }
+                else{
+                    if(book.no_copies <= 0){
+                        error = true;
+                        errorMSG = "Brak dostępnych egzemplarzy do wypożyczenia";
+                    }
+                    else if(!error){
+                        errorMSG = "Znaleziono: " + String(book._id);
+                    }
+                }
+        }
+       
+    
+    var bookArgs = {
+        'error': error,
+        'errorMSG': errorMSG,
+        'book': book,
+    }
+    response.json(bookArgs);
+});
+
+app.post('/deleteBookFromBasket', async function (request, response, next) {
+    const client = new MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2');
+    await client.connect();
+    const db = client.db('AGH');
+    const books = db.collection('books');
+    const students = db.collection('students');
+
+    var bookID = request.body.id;
+    var book = await books.findOne({'_id': bookID});
+    response.send({'book' : book});
+});
+
+app.get('/rentedBooks', async function (request, response, next) {
+    const client = new MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2');
+    await client.connect();
+    const db = client.db('AGH');
+    const books = db.collection('books');
+    const students = db.collection('students');
+    const rentals = db.collection('rentals');
+    
+    var error = false;
+    var errorMSG = '';
+
+    var rentedBooks = []
+    var student = await students.findOne({"firstname": firstname, "lastname": lastname});
+    if(!student){
+        error = true;
+        errorMSG = "Nie znaleziono użytkownika"
+    }
+    else{
+        var rents = await rentals.find({"student_id": student._id}).toArray();
+        for (let i = 0; i < rents.length; i++) {
+            var rental = rents[i];
+            var book = await books.findOne({ "_id": rental.book_id });
+        
+            if (rentedBooks.length === 0) {
+              var newPos = {
+                'book': book,
+                'no_copies': 1,
+              };
+              rentedBooks.push(newPos);
+            } else {
+              const foundBook = rentedBooks.find(obj => obj.book._id === book._id);
+              if (foundBook) {
+                foundBook.no_copies += 1;
+              } else {
+                var newPos = {
+                  'book': book,
+                  'no_copies': 1,
+                };
+                rentedBooks.push(newPos);
+              }
+            }
+          }
+    }
+
+    response.render('rented', {"error": error, "errorMSG": errorMSG, "rentedBooks": rentedBooks});
+});
+
+app.post('/updateRentedList', async function(request, response, next) {
+    const client = new MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2');
+    await client.connect();
+    const db = client.db('AGH');
+    const books = db.collection('books');
+    const students = db.collection('students');
+    const rentals = db.collection('rentals');
+    
+    var error = false;
+    var errorMSG = '';
+
+    var rentedBooks = []
+    var student = await students.findOne({"firstname": firstname, "lastname": lastname});
+    if(!student){
+        error = true;
+        errorMSG = "Nie znaleziono użytkownika"
+    }
+    else{
+        var rents = await rentals.find({"student_id": student._id}).toArray();
+        for (let i = 0; i < rents.length; i++) {
+            var rental = rents[i];
+            var book = await books.findOne({ "_id": rental.book_id });
+        
+            if (rentedBooks.length === 0) {
+              var newPos = {
+                'book': book,
+                'no_copies': 1,
+              };
+              rentedBooks.push(newPos);
+            } else {
+              const foundBook = rentedBooks.find(obj => obj.book._id === book._id);
+              if (foundBook) {
+                foundBook.no_copies += 1;
+              } else {
+                var newPos = {
+                  'book': book,
+                  'no_copies': 1,
+                };
+                rentedBooks.push(newPos);
+              }
+            }
+          }
+    }
+    response.send({"rentedBooks": rentedBooks})
+});
+
+app.post('/returnBook', async function (request, response, next) {
+    const client = new MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2');
+    await client.connect();
+    const db = client.db('AGH');
+    const books = db.collection('books');
+    const students = db.collection('students');
+    const rentals = db.collection('rentals');
+                            
+    var error = false;
+    var errorMSG = '';
+ 
+    var id = parseInt(request.body.id);
+    console.log(id);
+    const student = await students.findOne({'firstname': firstname, 'lastname': lastname});
+        if(!student){
+            error = true;
+            errorMSG = "Podany student nie znajduje się w naszej bazie";
+        }
+        else{
+            const book = await books.findOne({'_id': id})
+            console.log(book);
+                if(!book){
+                    error = true;
+                    errorMSG = "Podanej książki nie ma w naszej bazie";
+                }
+                else{
+                    const rental = await rentals.findOne({'book_id': book._id, 'student_id': student._id});
+                    if(!rental){
+                        error = true;
+                        errorMSG = "Podany student nie wypożyczał podanej książki";
+                    }
+                    if(!error){
+                       await rentals.deleteOne(rental);
+                       await books.updateOne(
+                        { '_id': book._id },
+                        { $set: { 'no_copies': book.no_copies+1 } }
+                      );
+                    }
+                }
+        }
+    response.send({"error": error, "errorMSG": errorMSG, "id": id});
+});
+
+/* ************************************************ */
+
+app.listen(8000, function () {
+    console.log('The server was started on port 8000');
+    console.log('To stop the server, press "CTRL + C"');
+});          
